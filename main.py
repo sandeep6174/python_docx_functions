7db431eeda3aeece3e1e3169402933f225eaddcb
sandeep6174 <68998192+sandeep6174@users.noreply.github.com>
2022-05-08 21:05:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from docx_functions import *
+
+if __name__=='__main__':
+    document = new_document()
+    add_paragraph_to_doc(document, 'hi', 'number', font_size=15)
+    add_paragraph_to_doc(document, 'hi', 'number', font_size=20)
+    paragraph1 = add_paragraph_to_doc(document, 'hi', font_name='Arial')
+    continue_para_in_doc(paragraph1, 'hold', True, underline=True)
+    add_hyperlink_to_doc(paragraph1, "fds", 'google.com')
+    tab=add_table_to_doc(document,3,3)
+    edit_table_in_doc(tab,'dfgh',1,1)
+    document.save('test.docx')
